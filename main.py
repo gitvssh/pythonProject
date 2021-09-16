5378a5ea449c277025be5113a65c3af4e731fc46
sh <gmavsks@gmail.com>
2021-09-16 21:12:56 +0900

    리스트 일일 5분코딩

--- main.py
+++ main.py
@@ -7,25 +7,26 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#python 주석
+
+
+# python 주석
 """
 이건 주석블럭
-주석 2
 """
-a=5000%3
+a = 5000 % 3
 print("a : ")
 print(a)
 
-b=20
-c=a/b
+b = 20
+c = a / b
 print(c)
 
 print(type(10))
 
-#20210909 List
-squares = [1,4,9,16,25]
+# 20210909 List
+squares = [1, 4, 9, 16, 25]
 print(squares[0])
-print(squares[-3:])#리스트 뒤로꺼내기 역방향으로 진행
+print(squares[-3:])  # 리스트 뒤로꺼내기 역방향으로 진행
 print(squares[:])
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -34,6 +35,29 @@ if __name__ == '__main__':
 watch = 100000
 book = 30000
 
-print(watch+book)
+print(watch + book)
+f = 40
+
+
+def abc(f):
+    c = 10
+    d = 20
+    print(c + d+f)
+
+
+abc(50)
+
+
+class Human:
+    def walk(self):
+        print("walking")
+    def run(self):
+        print("run")
+
+
+hum = Human
+hum.walk()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
