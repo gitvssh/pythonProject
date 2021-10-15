88aab7b362c1a278f99321312f4d70e08661c355
sh <gmavsks@gmail.com>
2021-10-15 21:36:35 +0900

    일일 5분코딩

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import pandas as pd
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
