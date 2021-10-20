dea9ed17d06da0a7a68919d5deac7b780fe56fbc
sh <gmavsks@gmail.com>
2021-10-20 21:39:06 +0900

    일일 5분코딩

--- main.py
+++ main.py
@@ -6,140 +6,23 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# python 주석
 """
-이건 주석블럭
+python 주석
+변수 할당
+함수 선언
+클래스 선언
+파이선 자료구조
+  리스트
+  튜플
+  딕셔너리
+조건문
+  for
+  while
+  if
+예외처리
 """
-a = 5000 % 3
-print("a : ")
-print(a)
-
-b = 20
-c = a / b
-print(c)
-
-print(type(10))
-
-# 20210909 List
-squares = [1, 4, 9, 16, 25]
-print(squares[0])
-print(squares[-3:])  # 리스트 뒤로꺼내기 역방향으로 진행
-print(squares[:])
-print("역방향 ")
-print(squares[-2])
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-watch = 100000
-book = 30000
-
-print(watch + book)
-f = 40
-
-test_list = ['one', 'two', 'three']
-for i in test_list:
-    print(i)
-
-
-class Human:
-    def walk(self):
-        print("he's animal, such a animal")
-
-    def run(self):
-        print("run")
-
-
-class Cat:
-    def walk(self):
-        print("cat cat cat")
-
-    def cry(self):
-        print("meow")
-
-
-hum = Human()
-hum.walk()
-hum.run()
-
-cat = Cat()
-cat.walk()
-cat.cry()
-
-g = a ** b
-print(g)
-
 # help("modules")
-
-dct = dict(a=[1, 2], b=[4, 5])
-key = 'c'
-try:
-    print(dct[key])
-except:
-    print("Key %s is missing. Add it with empty value" % key)
-    dct['c'] = [4, 5, 6]
-
-a = range(1, 10)
-
-print(a)
-
-
-class CatCat:
-    def meow(self):
-        print("meow")
-
-    def eat(self):
-        print("nyam")
-
-
-catcat = CatCat()
-
-catcat.meow()
-catcat.eat()
-# list
-listType = [1, 2, 3]
-# tuple
-tupleType = (1, 2, 3)
-# dictionaly
-dicType = {'dad': 'homer', 'mom': 'marge', 'size': 6}
-
-print(dicType['dad'])
-print(listType)
-
-lista = [1, 2, 3]
-tuplea = (1, 2, 3)
-dicttionarya = {'1': 'b', '2': 'c'}
-
-for l in lista:
-    print(l)
-    print("파이썬")
-
-print(lista)
-print(tuplea)
-print(dicttionarya['1'])
-
-for l in lista:
-    print(l)
-    print("파파파")
-
-
-# import numpy as np
-
-def fire():
-    print("fire")
-
-
-class testClass:
-    def __init__(self):
-        self.abc = "0"
-
-    def abs(self):
-        print("abc" + self.abc)
-
-
-t = testClass();
-t.abs()
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
