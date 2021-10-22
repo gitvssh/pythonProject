e3d9147647235f8dbf808f61a0b2281bfa44df68
sh <gmavsks@gmail.com>
2021-10-22 22:21:59 +0900

    일일 5분코딩

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
 """
 python 주석
 변수 할당
@@ -29,4 +31,12 @@ if __name__ == '__main__':
 
 a = 10
 b = "a"
-print(a,b)
+print(a, b)
+
+
+def abc(t, d):
+    print(t)
+    print(d)
+
+
+abc(1, 2)
