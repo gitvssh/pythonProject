c65edd30ec8b4817667f74c7e7800ddc6270c840
sh <gmavsks@gmail.com>
2021-11-17 21:51:11 +0900

    일일 5분코딩

--- main.py
+++ main.py
@@ -63,10 +63,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 z = np.linspace(-1, 1, 100)
-w = np.sqrt(1-z**2)
-print(w)
 x = np.linspace(0, 2, 100)
 plt.figure()
 plt.plot(x)
