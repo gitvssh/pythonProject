fe83d3a290721e16f794b9b393d3aef86fe9eb62
sh <gmavsks@gmail.com>
2021-11-19 22:19:24 +0900

    일일 5분코딩

--- main.py
+++ main.py
@@ -42,19 +42,3 @@ def abc(t=3, d=5):
     print(t)
     print(d)
 
-
-class testClass:
-    def testClass(self):
-        pass
-
-    def __init__(self, attr1="basic1", attr2="basic2"):
-        self.attr2 = attr2
-        self.attr1 = attr1
-        print("initiating")
-
-    def print(self, msg="basic"):
-        print("print msg : ", msg)
-        print("my attr is ", self.attr1, ",", self.attr2)
-
-    def set1(self, attr1):
-        self.attr1 = attr1
